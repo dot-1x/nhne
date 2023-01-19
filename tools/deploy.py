65f797488d5d6338ebed0a9a0d8330496277c005
dot-1x <mugiwaraluffy2003@gmail.com>
2023-01-20 00:33:36 +0700

    changed fix pipe to return new Deploy

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,6 +1,4 @@
-import asyncio
 import math
-from concurrent.futures import ThreadPoolExecutor
 from itertools import chain, islice, permutations
 from threading import Thread
 from typing import List, Tuple
@@ -11,7 +9,7 @@ from .utils import check_connected, TDeploy, get_best, get_combos
 from .data import MAX_NINJAS
 
 __all__ = ["Deploy"]
-TRESULT = List[Tuple[int, TDeploy]]
+TRESULT = List[Tuple[int, Tuple[TDeploy]]]
 
 
 class Deploy:
@@ -24,25 +22,25 @@ class Deploy:
         if not ignore_dupe and dupes:
             raise ValueError(f"Duplicate Ninja {dupes} Detected!")
 
-        self.n = 1000
-        self.permlen = math.perm(len(ninjas)) / self.n
+        self.permlen = math.perm(len(ninjas))
         self.permutate = permutations(ninjas)
 
-        conn, _ = check_connected(ninjas, main_ninjas)
-        self.current_pipe = conn
+        self.current_pipe, _ = check_connected(ninjas, main_ninjas)
         self.main = main_ninjas
         self.ninjas = ninjas
         self.rows = (ninjas[:5], (ninjas[5], *main_ninjas, ninjas[6]), ninjas[7:])
 
-    def thread_fix(self, deep=False):
+    def fix_pipe(self, deep=False):
         jobs: List[Thread] = []
         res: TRESULT = []
-        for x in range(self.n):
+        n = 1000
+        permlen = self.permlen/n
+        for x in range(n):
             job = Thread(
                 target=get_best,
                 args=[
                     res,
-                    self.permutate if deep else islice(self.permutate, int(self.permlen * x), int(self.permlen * (x + 1))),
+                    self.permutate if deep else islice(self.permutate, int(permlen * x), int(permlen * (x + 1))),
                     self.main,
                     self.current_pipe,
                     deep
@@ -53,7 +51,9 @@ class Deploy:
 
         for job in jobs:
             job.join()
-        return max(res, key=lambda n: n[0])
+
+        _, ninjas = max(res, key=lambda n: n[0])
+        return Deploy((*ninjas[0], ninjas[1][0], ninjas[0][-1], *ninjas[-1]), (ninjas[1][1:4]))
 
     def get_combos(self):
         combs = set(list(chain.from_iterable(n.get_available_combos() for n in self.main + self.ninjas)))
